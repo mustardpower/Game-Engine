77f1eaac0cd028405d8b1bcaf0286c07b2641a75
mustardpower <paul_gothard93@hotmail.co.uk>
2020-03-05 11:54:10 +0000

    add console logging

--- QtPhysicsEngine/main.cpp
+++ QtPhysicsEngine/main.cpp
@@ -3,6 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+
+    #ifdef _WIN32
+    if (AttachConsole(ATTACH_PARENT_PROCESS) || AllocConsole()) {
+        freopen("CONOUT$", "w", stdout);
+        freopen("CONOUT$", "w", stderr);
+    }
+    #endif
+
     QApplication a(argc, argv);
     QtPhysicsEngine w;
     w.show();
